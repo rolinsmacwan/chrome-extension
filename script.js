c5835ac087ee1588f8583ca09fe45c10e3cc4b88
Rolins Macwan <macwanrolins4567@gmail.com>
2022-09-13 01:26:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onload = startingMemes();
+
+const images =[];
+const memesId = document.getElementById('memesId');
+
+
+document.getElementById("forword").addEventListener("click", forword);
+document.getElementById("backword").addEventListener("click", backword);
+// document.getElementById("memesId").addEventListener("onerror", errorSolve);
+memesId.onerror = function() {errorSolve()};
+
+
+function startingMemes() {
+    fetch('https://meme-api.herokuapp.com/gimme/15')
+        .then(response => response.json())
+        .then(body => {
+            var count = body.count;
+            for (let i = 0; i < count; i++) {
+                images[i] = body.memes[i].preview[2]
+            }
+        })
+    }
+
+let i = 0;
+
+function forword(){
+    i++;
+    if(i>images.length - 1){
+        i = 0;
+    }
+    memesId.src = images[i];
+    console.log(memesId.src)
+}
+
+function backword(){
+    i--;
+    if(i<0){
+        i = images.length-1;
+    }
+    memesId.src = images[i];
+    console.log(memesId.src)
+}
+
+function errorSolve(){
+    memesId.src=images[0];
+}
+
+const image = document.querySelectorAll("img");
+
+image.forEach((image) => {
+  let imgsrc = image.getAttribute("src");
+  if (imgsrc === "") {
+    image.src = images[0];
+  }
+});
